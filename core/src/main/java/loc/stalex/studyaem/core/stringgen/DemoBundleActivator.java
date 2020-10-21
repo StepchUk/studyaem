1feffce9915665d483d36ac75f6ad8e6c753db3c
StepchUk <stepchyk@gmail.com>
2020-10-21 16:39:21 +0300

    Bundle Life Cycle: STARTING. Emulation created by TimeUnit

--- core/src/main/java/loc/stalex/studyaem/core/stringgen/DemoBundleActivator.java
+++ core/src/main/java/loc/stalex/studyaem/core/stringgen/DemoBundleActivator.java
@@ -5,12 +5,16 @@ import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.TimeUnit;
+
 public class DemoBundleActivator implements BundleActivator {
     private Logger logger = LoggerFactory.getLogger(DemoBundleActivator.class);
 
     @Override
     public void start(BundleContext bundleContext) throws Exception {
-        throw new IllegalStateException("Error +++");
+        logger.error("Starting activation");
+        TimeUnit.SECONDS.sleep(30);
+        logger.error("Activation performed");
     }
 
     @Override
